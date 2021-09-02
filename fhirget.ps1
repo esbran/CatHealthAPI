920b867daaf046b63580f67f72ebfeff274f74bb
Espen Brandt-Kjelsen <espen@microsoft.com>
2021-09-02 16:02:25 +0200

    added dicom

--- fhirget.ps1
+++ fhirget.ps1
@@ -1,5 +1,5 @@
 
-$fhirservice = 'https://esp1-esp1fhir.fhir.azurehealthcareapis.com'
+$fhirservice = 'https://eshealthapi-espenhealthfhir.fhir.azurehealthcareapis.com'
 $token = (Get-AzAccessToken -ResourceUrl $fhirservice).token
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
@@ -13,10 +13,17 @@ Write-Host $FhirGetPatient
 
 $FhirGetPatientName = Invoke-RestMethod "$fhirservice/Patient?name:exact=Espen" `
     -Method 'GET' `
-    -Headers $headers 
-Write-Host $FhirGetPatientName
+    -Headers $headers
+$FhirGetPatientName | ConvertTo-Json 
+Write-Host $FhirGetPatientName | ConvertTo-Json 
 
 $FhirGetPatientId = Invoke-RestMethod "$fhirservice/Patient/8fee45ff-cc41-4b22-a8d2-252d39f22b2e" `
+    -Method 'GET' `
+    -Headers $headers 
+    $FhirGetPatientId | ConvertTo-Json  
+Write-Host $FhirGetPatientId
+
+$FhirGetPatientId = Invoke-RestMethod "$fhirservice/$export?_container='https://eshealthapiyroratzfvoj4w.blob.core.windows.net/lake'" `
     -Method 'GET' `
     -Headers $headers 
     $FhirGetPatientId | ConvertTo-Json  
--- run.ps1
+++
@@ -1,8 +0,0 @@
-
-#Remove-AzResourceGroup -Name tempHealth 
-#New-AzResourceGroup -Name tempHealth -Location westeurope
-
-#New-AzResourceGroupDeployment -ResourceGroupName tempHealth -TemplateFile ./health.bicep
-#New-AzResourceGroupDeployment -ResourceGroupName tempHealth -TemplateFile ./testing.bicep
-
-New-AzDeployment -Location "westeurope" -TemplateFile "./healthmain.bicep" #-TemplateParameterFile "./health.parameters.json" -TemplateVersion "2.1"
